ac80a49e0199bc4865afee58575fbf7e4a3ea5e2
Ana Clara Pioto <155178079+anapioto@users.noreply.github.com>
2024-04-22 15:48:03 -0300

    Create 1- variaveis-e-constantes.js

---
+++ 1- variaveis-e-constantes.js
@@ -0,0 +1,15 @@
+console.log("Digita o nome do seu jogador")
+
+//declara uma variável
+let nickname = "Ana"
+
+//concantenando uma mensagem fixa + uma variável
+console.log("Bem vinda " + nickname)
+console.log(nickname + " entrou no servidor")
+
+//declarando constantes
+const notificacao = "Pokemon Go diz: "
+
+//saida
+console.log(notificacao + "tem um novo pokemon na região")
+console.log(notificacao + "você foi derrotado por um líder")
