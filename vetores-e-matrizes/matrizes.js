bbc291b7b7dd9bf85eeeb857c8db5a7404bd6058
Ana Clara Pioto <155178079+anapioto@users.noreply.github.com>
2024-04-22 23:07:56 -0300

    Add files via upload

---
+++ vetores-e-matrizes/matrizes.js
@@ -0,0 +1,9 @@
+//armário bidimensional - com linhas e colunas
+
+let cozinha = [
+    ["Café", "Pilão", "250g"],
+    ["Arroz", "Camil", "5kg"]
+  ]
+  
+  console.log ("Na minha cozinha tem " + cozinha[0][0], "da marca " + cozinha[0][1], "de " + cozinha[0][2])
+  console.log("Na minha cozinha tem " + cozinha [1][0], "da marca " + cozinha[1][1], "de " + cozinha[1][2])
---
+++ vetores-e-matrizes/vetores.js
@@ -0,0 +1,7 @@
+//para não criar várias variáveis, utilizamos os vetores
+//eles são como "armários", arnazenando os dados (geralmente do mesmo tipo)
+
+let cozinha = ["Arroz", "Feijão", "Açúcar"]
+// os vetores se iniciam pelo 0
+console.log(cozinha[0])
+
