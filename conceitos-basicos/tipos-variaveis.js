cb11cf939a12065fe60c1e9dc84022c0f1e424f9
Ana Clara Pioto <155178079+anapioto@users.noreply.github.com>
2024-04-22 23:07:38 -0300

    Add files via upload

---
+++ conceitos-basicos/tipos-variaveis.js
@@ -0,0 +1,16 @@
+//string - são variaveis que armazenam texto
+
+//number - são variaveis que armazenam números**
+
+//boolean - são variaveis lógicas, que armazenam o valor de ligado ou desligado (true/false)
+
+//string
+let nomePokemon = "pikachu"
+let pokemonSexo = "M"
+
+//number
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+
+//boolean
+let selecionavel = false
